e3289dcb5afc7d03f5515c97f35fbdf773e7e962
Caleb King <caleb.king536@gmail.com>
2019-10-29 15:00:32 -0600

    removed some extra unneeded comments

--- index.js
+++ index.js
@@ -238,21 +238,3 @@ const handleShoppingList = function () {
 
 // when the page loads, call `handleShoppingList`
 $(handleShoppingList);
-
-
-//TEMP
-
-//<input type="text" name="shopping-list-entry" class="js-shopping-list-entry" value="PREVIOUS VALUE">
-
-//<div class="shopping-item-controls">
-// <button class="shopping-item-toggle js-item-toggle">
-// <span class="button-label">check</span>
-// </button>
-// <button class="shopping-item-delete js-item-edit">
-// <span class="button-label">edit</span>
-// </button>
-
-// <button class="shopping-item-delete js-item-delete">
-// <span class="button-label">delete</span>
-// </button>
-// </div>
